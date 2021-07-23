d6b54168f92cd6fd6136051b0ebf981b62eba39c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-07-24 00:17:18 +0200

    Rename snap_value() to snap_resolution()

--- Status.pm
+++ Status.pm
@@ -105,7 +105,7 @@ sub _process {
 	$self->{'_current_layer'} = $h->actual_layer;
 	$self->{'_current_color'} = $h->actual_color;
 
-	my $rev_snap_resolution = reverse $h->snap_value;
+	my $rev_snap_resolution = reverse $h->snap_resolution;
 	$self->{'_snap_resolution'} = unpack_double_be($rev_snap_resolution);
 	# TODO Get
 	$self->{'_grid_value'} = 0.25;
